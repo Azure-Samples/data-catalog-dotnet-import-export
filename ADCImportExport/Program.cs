e9e0e5edcd79ea154ee1200c9894cc14bacc31c0
dvana <DerrickV@microsoft.com>
2016-01-13 11:17:03 -0800

    Azure Data Catalog Import/Export sample
    The Import/Export tool is a sample about how to use the Azure Data
    Catalog REST API to fetch information from the Azure Data Catalog and
    how to register items with the catalog.

---
+++ ADCImportExport/Program.cs
@@ -0,0 +1,466 @@
+﻿
+using System;
+using System.Text;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Security;
+using Microsoft.IdentityModel.Clients.ActiveDirectory; //Install-Package Microsoft.IdentityModel.Clients.ActiveDirectory
+
+
+namespace ADCImportExport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                System.Console.WriteLine("-import file_path or -export file_path [-search search_string]");
+                return;
+            }
+
+            string operation = args[0];
+            string filePath = args[1];
+
+            AzureDataCatalog td = new AzureDataCatalog(null, null);
+
+            if (operation.Equals("-export"))
+            {
+                string searchString = "";
+
+                if ((args.Length >= 4) && (args[2].ToLower().Equals("-search")))
+                {
+                    searchString = args[3];
+                }
+
+                using (StreamWriter sw = new StreamWriter(filePath, false))
+                {
+                    int count = Export(td,sw, searchString);
+                    System.Console.WriteLine("Items Exported: " + count);
+                }
+                
+            }
+            else if (operation.Equals("-import"))
+            {
+                Import(td, filePath);
+            }
+            else
+            {
+                System.Console.WriteLine("Must specify either import or export");
+            }
+        }
+
+        static int Export(AzureDataCatalog td, StreamWriter sw, string searchString)
+        {
+            const int countPerPage = 100;
+
+            bool firstTime = true;
+            int startPage = 1;
+            int totalResultsCount = 0;
+            int totalExportedSuccessfully = 0;
+
+            sw.Write("{\"catalog\":[");
+
+            do
+            {
+                string results = td.Search(searchString, startPage, countPerPage);
+
+                if (results == null)
+                { 
+                    return 0;
+                }
+
+                if (firstTime)
+                {
+                    totalResultsCount = (int)JObject.Parse(results)["totalResults"];
+                }
+
+                var assetList = JObject.Parse(results)["results"].Children();
+
+                foreach (JObject asset in assetList["content"])
+                {
+                    if (firstTime)
+                    {
+                        firstTime = false;
+                    }
+                    else
+                    {
+                        sw.Write(",");
+                    }
+                    asset.Remove("__permissions");
+                    asset.Remove("__effectiveRights");
+                    asset.Remove("__roles");
+                    asset.Remove("__type");
+
+                    //Set contributor equal to "Everyone" on all nodes. This allows them to be updated by others later.  Ideally we would preserve the contributor but that requires
+                    //a special platform to enable it.
+
+                    JToken contributor = JObject.Parse("{'role': 'Contributor','members': [{'objectId': '00000000-0000-0000-0000-000000000201'}]}");
+                    JArray roles = new JArray();
+                    roles.Add(contributor);
+                    asset.Add("__roles", roles);
+
+                    bool previewExists = asset.Remove("previews");
+                    bool columnsDataProfilesExists = asset.Remove("columnsDataProfiles");
+                    bool tableDataProfilesExist = asset.Remove("tableDataProfiles");
+
+                    if (previewExists || columnsDataProfilesExists || tableDataProfilesExist)
+                    {
+                        var fullAsset = JObject.Parse(td.Get(asset["__id"].ToString()));
+                        if (previewExists)
+                        {
+                            asset.Add("previews", fullAsset["previews"]);
+                        }
+                        if (columnsDataProfilesExists)
+                        {
+                            asset.Add("columnsDataProfiles", fullAsset["columnsDataProfiles"]);
+                        }
+                        if (tableDataProfilesExist)
+                        {
+                            asset.Add("tableDataProfiles", fullAsset["tableDataProfiles"]);
+                        }
+                    }
+
+                    StripSystemProperties(asset.Children());
+
+                    sw.Write(JsonConvert.SerializeObject(asset));
+                    totalExportedSuccessfully++;
+                    if (totalExportedSuccessfully % 10 == 0)
+                    {
+                        Console.Write(".");
+                    }
+                }
+
+                startPage++;
+            } while ((startPage - 1) * countPerPage < totalResultsCount);
+
+            sw.Write("]}");
+
+            Console.WriteLine("");
+
+            return totalExportedSuccessfully;
+        }
+
+        static void Import(AzureDataCatalog td, string exportedCatalogFilePath)
+        {
+            int totalAssetsImportSucceeded = 0;
+            int totalAssetsImportFailed = 0;
+
+            System.IO.StreamReader sr = new StreamReader(exportedCatalogFilePath);
+            JsonTextReader reader = new JsonTextReader(sr);
+
+            StringWriter sw = new StringWriter(new StringBuilder());
+
+            JsonTextWriter jtw = new JsonTextWriter(sw);
+
+
+            reader.Read();
+            if (reader.TokenType != JsonToken.StartObject)
+            {
+                throw new Exception("Invalid Json. Expected StartObject");
+            }
+
+            reader.Read();
+            if ((reader.TokenType != JsonToken.PropertyName) || (!reader.Value.ToString().Equals("catalog")))
+            {
+                throw new Exception("Invalid Json. Expected catalog array");
+            }
+
+            reader.Read();
+            if (reader.TokenType != JsonToken.StartArray)
+            {
+                throw new Exception("Invalid Json. Expected StartArray");
+            }
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonToken.EndArray)
+                    break;
+
+                jtw.WriteToken(reader);
+
+                JObject asset = JObject.Parse(sw.ToString());
+
+                string id = asset["__id"].ToString();
+                asset.Remove("__id");
+                string[] idInfo = id.Split(new char[] { '/' });
+                string newid;
+
+                string UpdateResponse = td.Update(asset.ToString(), idInfo[0], out newid);
+
+                if ((UpdateResponse != null) && (!string.IsNullOrEmpty(newid)))
+                {
+                    totalAssetsImportSucceeded++;
+
+                    if (totalAssetsImportSucceeded % 50 == 0)
+                    {
+                        System.Console.WriteLine(totalAssetsImportSucceeded  + "Assets Imported Succesfully");
+                    }
+                }
+                else
+                {
+                    totalAssetsImportFailed++;
+                }
+
+                //reset local variables for next iteration
+                sw = new StringWriter(new StringBuilder());
+                jtw = new JsonTextWriter(sw);
+
+            }
+
+            Console.WriteLine("Total Imported Success: " + totalAssetsImportSucceeded);
+            Console.WriteLine("Total Imported Failed: " + totalAssetsImportFailed);
+        }
+
+
+
+        static void StripSystemProperties(JEnumerable<JToken> children)
+        {
+            foreach (JToken child in children)
+            {
+                if (child.Type.ToString().Equals("Property"))
+                {
+
+                }
+                else if (child.Type.ToString().Equals("Array"))
+                {
+
+                }
+                else if (child.Type.ToString().Equals("Object"))
+                {
+                    JObject obj = (JObject)child;
+                    obj.Remove("__permissions");
+                    obj.Remove("__effectiveRights");
+                    bool retVal = obj.Remove("__roles");
+                    obj.Remove("__type");
+                    obj.Remove("__id");
+
+                    //If there was a roles object then replace it.
+                    if (retVal)
+                    {
+                        //Set contributor equal to "Everyone" on all nodes. This allows them to be updated by others later.  Ideally we would preserve the contributor but that requires
+                        //a special platform to enable it.
+                        JToken contributor = JObject.Parse("{'role': 'Contributor','members': [{'objectId': '00000000-0000-0000-0000-000000000201'}]}");
+                        JArray roles = new JArray();
+                        roles.Add(contributor);
+                        obj.Add("__roles", roles);
+                    }
+
+                    if ((obj["__creatorId"] != null) && (obj["__creatorId"].ToString().Contains("@")))
+                    {
+                        obj["__creatorId"] = "imported_" + obj["__creatorId"].ToString();
+                    }
+                    
+
+                }
+                else
+                {
+                    return;
+                }
+
+                StripSystemProperties(child.Children());
+            }
+        }
+    }
+
+    public class AzureDataCatalog
+    {
+        private readonly string ClientId;
+        private readonly Uri RedirectUri;
+        private readonly string ResourceId;
+        private readonly string CatalogName;
+
+        private readonly AuthenticationResult auth;
+        private static readonly AuthenticationContext AuthContext = new AuthenticationContext("https://login.windows.net/common/oauth2/authorize");
+
+        public AzureDataCatalog(string userName, string password)
+        {
+            //NOTE: You must fill in the App.Config with the following three settings. The first two are values that you received registered your application with AAD. The 3rd setting is always the same value.:
+            //  <appSettings>
+            //    <add key="ClientId" value=/>
+            //    <add key="RedirectURI" value=/>
+            //    <add key="ResourceId" value="https://datacatalog.azure.com"/>
+            //  </appSettings>
+
+            ClientId = System.Configuration.ConfigurationManager.AppSettings["ClientId"];
+            RedirectUri = new Uri(System.Configuration.ConfigurationManager.AppSettings["RedirectURI"]);
+            ResourceId = System.Configuration.ConfigurationManager.AppSettings["ResourceId"];
+            CatalogName = "DefaultCatalog";
+
+            if (userName == null)
+            {
+                auth = AuthContext.AcquireToken(ResourceId, ClientId, RedirectUri, PromptBehavior.Always);
+            }
+            else
+            {
+                var credentials = new UserCredential(userName, ToSecureString(password));
+                password = null;
+                auth = AuthContext.AcquireToken(ResourceId, ClientId, credentials);
+            }
+        }
+
+        public string Get(string id)
+        {
+            var fullUri = string.Format("https://{0}.datacatalog.azure.com/{1}/views/{2}?api-version=2015-07.1.0-Preview", auth.TenantId, CatalogName, id);
+
+            string requestId;
+            HttpWebRequest request = CreateHttpRequest(fullUri, "GET", out requestId);
+
+            try
+            {
+                string s = GetPayload(ref request);
+                return s;
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Status);
+                Console.WriteLine("Request Id: " + requestId);
+
+                if (ex.Response != null)
+                {
+                    // can use ex.Response.Status, .StatusDescription
+                    if (ex.Response.ContentLength != 0)
+                    {
+                        using (var stream = ex.Response.GetResponseStream())
+                        {
+                            using (var reader = new StreamReader(stream))
+                            {
+                                Console.WriteLine("Failed Get of asset: " + id);
+                                Console.WriteLine(reader.ReadToEnd());
+                            }
+                        }
+                    }
+                }
+                return null;
+            }
+        }
+
+        public string Search(string searchTerm, int startPage, int count)
+        {
+            var fullUri = string.Format("https://{0}.search.datacatalog.azure.com/{1}/search/search?searchTerms={2}&count={3}&startPage={4},&api-version=2015-06.0.1-Preview", auth.TenantId, CatalogName, searchTerm, count, startPage);
+
+            string requestId;
+            HttpWebRequest request = CreateHttpRequest(fullUri, "GET", out requestId);
+
+            try
+            {
+                string s = GetPayload(ref request);
+                return s;
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Status);
+                Console.WriteLine("Request Id: " + requestId);
+                if (ex.Response != null)
+                {
+                    // can use ex.Response.Status, .StatusDescription
+                    if (ex.Response.ContentLength != 0)
+                    {
+                        using (var stream = ex.Response.GetResponseStream())
+                        {
+                            using (var reader = new StreamReader(stream))
+                            {
+                                Console.WriteLine(reader.ReadToEnd());
+                            }
+                        }
+                    }
+                }
+                return null;
+            }
+        }
+
+        public string Update(string postPayload, string viewType, out string id)
+        {
+
+            var fullUri = string.Format("https://{0}.datacatalog.azure.com/{1}/views/{2}?api-version=2015-07.1.0-Preview", auth.TenantId, CatalogName, viewType);
+
+            string requestId;
+            var request = CreateHttpRequest(fullUri, "POST", out requestId);
+
+            try
+            {
+                request.ContentType = "application/json";
+
+                byte[] postData = System.Text.Encoding.UTF8.GetBytes(postPayload);
+                request.ContentLength = postData.Length;
+
+                Stream requestStream = request.GetRequestStream();
+
+                requestStream.Write(postData, 0, postData.Length);
+
+                var response = (HttpWebResponse)request.GetResponse();
+                var responseStream = response.GetResponseStream();
+
+                id = response.Headers["location"];
+
+                StreamReader reader = new StreamReader(responseStream);
+                return reader.ReadToEnd();
+
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Status);
+                Console.WriteLine("Request Id: " + requestId);
+                if (ex.Response != null)
+                {
+                    // can use ex.Response.Status, .StatusDescription
+                    if (ex.Response.ContentLength != 0)
+                    {
+                        using (var stream = ex.Response.GetResponseStream())
+                        {
+                            using (var reader = new StreamReader(stream))
+                            {
+                                Console.WriteLine("Failed Update of asset: " + postPayload.Substring(0,50));
+                                Console.WriteLine(reader.ReadToEnd());
+                            }
+                        }
+                    }
+                }
+                id = null;
+                return null;
+            }
+        }
+
+        private HttpWebRequest CreateHttpRequest(string url, string method, out string requestId)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = method;
+
+            string authHeader = auth.CreateAuthorizationHeader();
+            request.Headers.Add("Authorization", authHeader);
+            requestId = Guid.NewGuid().ToString();
+            request.Headers.Add("x-ms-client-request-id", requestId);
+
+            return request;
+        }
+
+        private string GetPayload(ref HttpWebRequest request)
+        {
+            string result = String.Empty;
+            var response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode != HttpStatusCode.OK)
+                throw new ApplicationException("Request wrong");
+            var stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            result = reader.ReadToEnd();
+            return result;
+        }
+
+        private static SecureString ToSecureString(string str)
+        {
+            var result = new SecureString();
+            foreach (var c in str.ToCharArray())
+            {
+                result.AppendChar(c);
+            }
+            result.MakeReadOnly();
+            return result;
+        }
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,56 @@
+# Import/Export sample tool
+
+The Import/Export tool is a sample about how to use the Azure Data Catalog REST API to fetch information from the Azure Data Catalog and how to register items with the catalog. It also shows how to manage a catalog.
+
+To get started using the sample, you first need to build the executable.  You need to do the following steps.
+
+-	Download the sample project.
+-	Open it in Visual Studio and fetch the dependent nuget packages.
+-	Create (or use an existing) Azure Active Directory Application Id and the corresponding information. If you do not have one and need instructions on how to get one see here.
+-	Add the following to the app config for the app where the first two values are the ones you got from the previous step.
+
+     <userSettings>
+        <ADCImportExport.Properties.Settings>
+            <setting name="ClientId" serializeAs="String">
+                <value />
+            </setting>
+            <setting name="RedirectURI" serializeAs="String">
+                <value />
+            </setting>
+            <setting name="ResourceId" serializeAs="String">
+                <value />
+            </setting>
+        </ADCImportExport.Properties.Settings>
+    </userSettings>Compile the app
+
+-	You are now ready to run it.
+
+The app can be run in one of two modes:
+
+**Export**
+
+In export mode you specify -export on the command line and state where you want the exported catalog metadata to go.  Here is an example of the command line:
+
+ADCImportExport.exe -export c:\temp\DemoCatalog.json
+
+The sample tool prompts for credentials (it uses the credentials to identify which catalog it should be exporting from).  Once you have authenticated, it pulls down all the assets from the catalog and stores it in the file location specified.  The file is a json format.  You can inspect the format to get a feel for how it is stored.  The set of information is slightly different than what you get from a search result as the format is setup to be able to easily import the catalog metadata back into the same catalog or a new catalog.
+
+There is also an optional parameter to scope the set of assets you want to export.  Use the -search parameter to specify a search query.  Only the results of the search query will be exported.  Here is an example of the command line:
+
+ADCImportExport.exe -export c:\temp\DemoCatalog.json -search tags=Demo
+
+This will export only the assets that have a tag with the value of Demo. (Note that equality searching is case sensitive so it won’t match assets with a tag equal to “demo” or “dEmo”.  
+
+**Import**
+
+In Import mode you specify -import on the command line and state where the imported catalog exists.  Here is an example of the command line:
+
+
+ADCImportExport.exe -import c:\temp\DemoCatalog.json
+
+The sample tool prompts for credentials (it uses the credentials to identify which catalog it should be exporting from).  Once you have authenticated, its starts pushing up all the assets found in the json file.
+
+**Caveats**
+
+The sample tool leverages the standard Data Catalog API for searching and registering items in the catalog.  This means that all items are created in the context of the person running the tool. So all notions of ownership both at the asset level (the owner) and the annotation level (the contributor) are lost when doing the export. Because there is no owner, the visibility restrictions are also lost.  When importing a catalog to another tenant this is necessary because none of the identities would exist in the new tenant anyway. Even if you are staying in the same tenant (say exporting, deleting the catalog in one region, recreating it in another region, and then importing) identities cannot be preserved as the annotation owner (i.e. the contributor) is set by the system to the user identity that is logged in when the API is called.  
+To simplify the behavior, all items are created with the contributor set to “Everyone” which is a special identity that means any user has permissions to edit or delete that item. (Whereas only the contributor on the original annotation in the original catalog had the power to edit or delete).  The sample tool also changes the creatorId to make this more explicit by putting the word imported_ on the front of the value so it is clear this is an imported item. Users can then copy over the imported metadata into their own new annotations for which they are the contributor.
